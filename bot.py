2105638edb3c9078e35278e37e3bcb08dc5721e7
harshad444 <zapadiyaharshad2002@gmail.com>
2025-07-11 10:58:52 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,9 +1,1 @@
-
-import telebot
-bot = telebot.TeleBot("7968402839:AAElc7pzA3PsSsmtr24b3wSmDMB1OzM-gVU")
-
-@bot.message_handler(commands=["start"])
-def start(message):
-    bot.reply_to(message, "Welcome to Tap Warrior!")
-
-bot.polling()
+# main bot code here
